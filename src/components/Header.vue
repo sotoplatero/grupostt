eb5b907d32877994535ca4c2ec61f57d4ba50276
Damián Soto Platero <soto.platero@gmail.com>
2022-11-03 19:32:23 -0300

    clear header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,28 @@
 <script setup>
 	import Logo from '@/components/Logo.vue'
 </script>
+
 <template>
-    <header class="flex items-center justify-between mb-12 py-3">
-    	<a href="/" class="flex items-center text-xl space-x-2 font-extrabold text-inherit">
+
+    <header>
+
+    	<a href="/" class="brand">
 	    	<Logo />
-	    	<span>Rick and Morty</span>	
+	    	<span >Rick and Morty</span>	
     	</a>
 
-        <nav>
-          <RouterLink to="/characters" class="border px-4 py-3 border-gray-600">
-	          <b>Characters</b>
+      <nav>
+        <RouterLink to="/characters">
+          Characters
 	      </RouterLink>
-        </nav>
+      </nav>
+
     </header>	
+
 </template>
 
 <style scoped>
-	nav, header {
-		@apply flex
-	}
+	header { @apply flex items-center justify-between mb-12 py-3}
+	a.brand { @apply flex items-center text-base leading-tight sm:text-xl space-x-2 font-extrabold text-inherit }
+	nav a { @apply border px-4 py-3 border-gray-600 font-semibold }
 </style>
