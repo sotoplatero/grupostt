cafcdc3fcdc26daf2e927fb78e7a0fede36e3968
Damián Soto Platero <soto.platero@gmail.com>
2022-11-03 18:02:25 -0300

    update home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,8 +4,23 @@
 
 <template>
   <main class="h-full grow flex items-center justify-center">
-    <div>
-      Damian Soto Platero
+    <div class="text-center">
+      <h1>Prueba Técnica</h1>
+      <p>Damián Soto Platero</p>
+      <ul class="mt-4">
+        <li><a href="https://grupostt.vercel.app">Servicio desplegado serverless (Vercel)</a></li>
+        <li><a href="https://grupostt.vercel.app">Servicio desplegado Docker</a></li>
+        <li><a href="https://grupostt.vercel.app">Repositorio Front</a></li>
+        <li><a href="https://grupostt.vercel.app">Repositorio Back</a></li>
+      </ul>
+      <a href="/characters" class="cta">
+        Comenzar
+      </a >
     </div>
   </main>
 </template>
+
+<style scoped>
+  h1{ @apply text-2xl font-bold}
+  a.cta{ @apply block bg-lime-600 text-white p-3 font-bold mt-6 border-lime-700 hover:bg-lime-700 }
+</style>
